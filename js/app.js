7b1b61622cb616ae6b5764da5911d8a808953981
Guillaume Pasquet <guillaume@tutorfair.com>
2013-05-04 19:07:13 +0100

    Working version.

--- js/app.js
+++ js/app.js
@@ -1,31 +1,39 @@
-// Nothing yet
+var store = null;
 
 /**
- * Just testing for localstorage support.
+ * Refreshes the items in the todo list.
  */
-function has_storage() {
-    try {
-        return 'localStorage' in window && window['localStorage'] !== null;
-    } catch (e) {
-        return false;
+function refreshItems() {
+    // Wiping all demo items.
+    $('#itemsList .list').html('');
+    // Loading storage items.
+    var items = store.getAllItems(true);
+    for(var key in items) {
+        var item = items[key];
+        $('#itemsList .list').append('<li id="' + item.id + '"class="item">'
+            + '<button id="'+ item.id +'" class="btnDelItem"></button>'
+            + '<p>' + item.value + '</p>'
+            + '</li>');
     }
 }
 
 $(document).ready(function() {
     // Do we have localstorage support? If not, forget it.
-    if(!has_storage()) {
-        alert("Sorry but you can't use this app without local storage support." +
-              " Please use a compatible browser.");
+    try {
+        store = new itemsStore();
+    }
+    catch(e) {
+        if(e == "No localStorage support.") { 
+            alert("Sorry but you can't use this app without local storage support." +
+                  " Please use a compatible browser.");
+        } else {
+            alert("An error occured. Please contact the developer.");
+            console.log(e);
+        }
+        return;
     }
 
-    // Wiping all demo items.
-    $('#itemsList .list').html('');
-    // Loading storage items.
-    for(var key in localStorage) {
-        $('#itemsList .list').append('<li id="' + key + '"class="item"><p>'
-                                     + localStorage.getItem(key)
-                                     + '</p></li>');
-    }
+    refreshItems();
 
     // An item was clicked.
     $('#itemsList .item').live('click', function(e) {
@@ -34,6 +42,29 @@ $(document).ready(function() {
 
     // The add button was clicked.
     $('#btnAdd').click(function(e) {
-        
+        $('#mainFrame').hide();
+        $('#addItemFrame').show();
+    });
+
+    // Hides the add item frame.
+    $('#btnMain').click(function(e) {
+        $('#addItemFrame').hide();
+        $('#mainFrame').show();
+    });
+
+    // Adds a new item.
+    $('#addItem').click(function(e) {
+        store.addItem($('#newItem').val());
+        $('#newItem').val('');
+        // Note that the items are refreshed while the form is shown.
+        refreshItems();
+        $('#addItemFrame').hide();
+        $('#mainFrame').show();
+    });
+
+    // Deletes an item.
+    $('.btnDelItem').live('click', function(e) {
+        store.delItem($(this).attr('id'))
+        refreshItems();
     });
 });
